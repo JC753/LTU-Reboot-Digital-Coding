9d05bdfc3d0126209eec7e98a2a2dffa94616c4e
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-25 10:29:55 +0000

    Update task-3.1.2.js
    Add alert, console.log and document.write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,8 @@
+alert("This is an alert message");
+
+/*
+Below is a message that will appear in the console 
+*/
+console.log("This is a message from the console");
+
+document.write("Hello from the document")
