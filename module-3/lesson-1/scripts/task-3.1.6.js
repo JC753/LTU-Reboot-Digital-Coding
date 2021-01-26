941390f2876484d944b5ea717924ecbbe1c73702
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-26 09:49:00 +0000

    Update task-3.1.6.js
    Updated task: different way of doing it

--- module-3/lesson-1/scripts/task-3.1.6.js
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -1,5 +1,17 @@
-var preTipTotal = 30;
-var tipPercent = 1.2;
-totalBill = preTipTotal * tipPercent;
+// var preTipTotal = 30;
+// var tipPercent = 1.2;
+// -totalBill = preTipTotal * tipPercent;
+// document.write(totalBill);
 
-document.write(totalBill);
+var mealPriceTip = 30;
+var tipAmount = 20
+
+var tipAmount = (mealPriceTip * tipAmount) / 100;
+
+var receipt = 'Your Total bill with tip is : ' + '£' + (mealPriceTip + tipAmount) + '<br>';
+
+document.write( receipt );
+
+var totalPrice = mealPriceTip + tipAmount;
+
+document.write ( 'Your tip amount was: ' + '£' + tipAmount.toFixed( 2) + '<br>' + totalPrice.toFixed(2));
