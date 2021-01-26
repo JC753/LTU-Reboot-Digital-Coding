6339bc5a496963e1b79ab40ee192986d162aeedd
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-26 11:32:08 +0000

    Update task-3.2.5.js
    add if and else if for temperature variable

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,15 @@
+let temperature = 31;
+
+if (temperature < 0)
+ {
+  document.write('Stay inside');
+ }
+
+else if (temperature < 30) {
+  
+  document.write('Wear a coat and hat');
+}
+
+else if ( temperature <50) {
+  document.write('Wear your coat');
+}
