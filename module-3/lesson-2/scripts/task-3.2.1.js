f26893a38d04e373d06f58e0600ad5b2765760bc
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-26 10:00:50 +0000

    Update task-3.2.1.js
    declare function parrot and invoke parrot function

---
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -0,0 +1,7 @@
+//Declare a function
+function parrotFacts() {
+  document.write('Some parrot species can live for over 80 years' + '<br>');
+  document.write('Kakapos are a critically endangered flightless parrot');
+}
+//Invoke a function
+parrotFacts();
