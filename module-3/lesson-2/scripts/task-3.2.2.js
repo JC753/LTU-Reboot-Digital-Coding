81765fa9a3f4d144ea2ba263911b981a1cf801e8
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-26 10:33:09 +0000

    Update task-3.2.2.js
    add function showFullName and invoke function

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,12 @@
+function showFullName(fName,sName){
+
+  var fullName = fName + " " + sName;
+
+  document.write(fullName +'<br>');
+
+}
+
+showFullName('Jimmy', 'Cape');
+showFullName('Jimmy', 'Cape');
+showFullName('Jimmy', 'Cape');
+showFullName('Jimmy', 'Cape');
