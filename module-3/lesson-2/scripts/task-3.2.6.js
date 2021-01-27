8b3ddb3ddf3a182d857b451401006e6929edec57
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-27 09:53:56 +0000

    Update task-3.2.6.js
    Added other seasons example as comments

--- module-3/lesson-2/scripts/task-3.2.6.js
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -6,3 +6,15 @@ document.write("You can work for us behind the bar!");
 } else {
   document.write("You are not eligible to work for us due to your age and barman experience.")
 }
+
+// Another way below
+// let weather = "spring";
+// let clothingChoice = "";
+
+// if (weather === "spring") {
+  // clothingChoice = "Put on rain boots";
+// } else if (weather ==="summer") {
+  // clothingChoice = "Make sure to take your sunscreen."
+// }
+
+// etc for the other 2 seasons
