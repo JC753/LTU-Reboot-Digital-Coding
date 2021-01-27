1df76ad7897103e8ee5bdf56ac736cc5aabd304c
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-27 09:47:40 +0000

    Update task-3.2.6.js
    add variables and logical operators.

---
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -0,0 +1,8 @@
+var age = 21;
+var barManExperience = 2;
+
+if (age >=20 && barManExperience > 1) {
+document.write("You can work for us behind the bar!");
+} else {
+  document.write("You are not eligible to work for us due to your age and barman experience.")
+}
