ecf1e1f1c8668cb3373f485a47c38fdb20ef9815
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-27 10:15:10 +0000

    Update task-3.3.1.js
    add functions, variables and return

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,22 @@
+function percentageCalculator (number, percentDiscount) {
+  let percentDiscount = ( number * percentDiscount ) / 100;
+  return percentDiscount;
+
+}
+
+  let percentDiscount = 3.5;
+  let number = 300;
+
+  document.write(percentageCalculator( 300 , 3.5 ) );
+
+
+// function percentageCalculator ( number, percentage ) {
+// let percentageOf = ( number * percentage ) / 100;
+// return percentageOf;
+// }
+
+// let percentage = 3.5;
+// let number = 100
+
+// document.write( percentageCalculator( 300 , 3.5 ) );
+// document.write( percentageCalculator( 640 , 14 ) );
