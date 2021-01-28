74d9c9fdddf4cc034d017c62932426738dcf1c95
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-01-28 10:49:17 +0000

    Create task-3.4.3.js
    add variable favouriteFastFood, array  and for loop.

---
+++ module-3/lesson-4/Scripts/task-3.4.3.js
@@ -0,0 +1,6 @@
+var favouriteFastFood = [ 'KFC', 'Maccies', 'Burger King', 'PizzaHut', ];
+
+for ( var index = 0; index < favouriteFastFood.length; index++) {
+
+  console.log(favouriteFastFood [index]);
+}
