cb1331940ac94630196ea846a588e9a28f461029
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-02-01 09:39:51 +0000

    Update task-3.4.5.js
    add letsCook function

---
+++ module-3/lesson-4/Scripts/task-3.4.5.js
@@ -0,0 +1,18 @@
+var easyRecipe = {
+  recipeTitle: 'Shakshuka',
+  servings: 2,
+  ingredients: ['Drizzle of olive oil', '2 white onions', 'garlic', '2 cans cherry tomatoes', 'bunch of coriander', '4 eggs'],
+  directions: ['Step 1: Heat the frying pan with a drizzle of olive oil and put the garlic, onions and chilli flakes - mix well for 5 mins. Add the tinned tomatoes and simmer for 8-10 mins', 
+  'Step 2: Crack the eggs and on top of the mixture and put a lid on. Simmer for 6-8 mins or until the eggs are to your likening.',
+  'Step 3: Scatter with coriander leaves and enjoy!']
+
+}
+
+var ingredients = easyRecipe.ingredients;
+
+for (var index = 0; index < ingredients.length; index++)
+
+console.log( ingredients[index]);
+
+eat: function(letsCook) {
+  console.log("I'm hungry!" + letsCook);
