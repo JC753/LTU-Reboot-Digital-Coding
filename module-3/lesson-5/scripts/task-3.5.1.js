915fe3c4ec7c0edf53a2a1acb7868b7584eac009
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-02-01 10:32:17 +0000

    Create task-3.5.1.js
    add function shoppingCartTotal, looped over an array of objects, adding price and quantity together

---
+++ module-3/lesson-5/scripts/task-3.5.1.js
@@ -0,0 +1,19 @@
+function shoppingCartTotal( array ) {
+
+
+  let totalPrice = 0;
+
+  for ( let index = 0; index < array.length; index++  ) {
+
+    totalPrice = totalPrice + array[ index ]
+  
+totalPrice = totalPrice + array[ index ].price * array[ index ].quantity;
+
+  }
+
+//step 4
+  return totalPrice.toFixed( 2 );
+
+} 
+
+console.log( shoppingCartTotal( shoppingCart ) );
