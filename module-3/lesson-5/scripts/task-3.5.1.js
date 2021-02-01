4da1c47f83021a318ff1de454433cf374a1f9181
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-02-01 11:20:31 +0000

    Update task-3.5.1.js

--- module-3/lesson-5/scripts/task-3.5.1.js
+++ module-3/lesson-5/scripts/task-3.5.1.js
@@ -1,8 +1,9 @@
+//step 1
 function shoppingCartTotal( array ) {
 
-
+//step 2
   let totalPrice = 0;
-
+//step 3
   for ( let index = 0; index < array.length; index++  ) {
   
 totalPrice = totalPrice + array[ index ].price * array[ index ].quantity;
