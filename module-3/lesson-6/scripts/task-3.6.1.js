d673379e6a81a8f343095160f2327e3070ded013
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-02-02 10:53:53 +0000

    Create task-3.6.1.js
    Select img and changed src attribute, selec secondParagraph and add styles

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,8 @@
+let logo = document.querySelector('img');
+logo.setAttribute('src', 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
+
+
+let secondParagraph = document.querySelectorAll('p')[1];
+secondParagraph.style.border = '4px solid goldenrod';
+secondParagraph.style.marginTop = '50px';
+secondParagraph.style.padding = '50px';
