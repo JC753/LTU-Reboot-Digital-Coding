ee10757e720a8c911abd4a15edcdbe4b27ed4313
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-02-02 11:14:37 +0000

    Create task-3.6.2.js
    select div, create paragraph  and append

---
+++ module-3/lesson-6/scripts/task-3.6.2.js
@@ -0,0 +1,7 @@
+let div = document.querySelectorAll('div')[0];
+
+let newParagraph = document.createElement('p');
+let newTextNode = document.createTextNode('This is the new text Node');
+
+    newParagraph.appendChild( newTextNode );
+    div.appendChild( newParagraph );
