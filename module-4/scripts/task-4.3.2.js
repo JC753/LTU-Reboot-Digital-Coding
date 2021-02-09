18baa07a51c0171ce2ce73971a66407bc239ef03
JC753 <77273466+life-of-leo@users.noreply.github.com>
2021-02-09 16:15:59 +0000

    Create task-4.3.2.js
    attempting reading list task using array method

---
+++ module-4/scripts/task-4.3.2.js
@@ -0,0 +1,37 @@
+/*
+Reading List
+
+Keep a track of which books you read and which books you want to read
+
+Show title of each book and author
+
+Depending on whether you've read the book, show the following message:
+
+You have read BOOK TITLE by AUTHOR
+
+You haven't read BOOK TITLE by AUTHOR. Why haven't you read it yet?
+*/
+
+/*
+
+1. Create list of books - Use Array
+2. Cycle through the list of books
+3. Show whether I've read the book or not.
+
+*/
+
+let books = [
+{
+  title: 'Game of Thrones'
+  author: 'George R.R. Martin'
+  Read: false,
+},
+{
+  title: 'Harry Potter'
+  author: 'J.K Rowling'
+  Read: false,
+},
+
+];
+
+for 
